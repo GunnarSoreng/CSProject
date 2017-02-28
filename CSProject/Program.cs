2f393ca4296c3e1c3b4ea870cf43499a02294071
Gunnar <Gunnar>
2017-02-28 15:24:30 +0100

    Inital version. Empty Solution

---
+++ CSProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProject
+{
+    // Final project from the book
+    // "Learn C# in one day and learn it well"
+    //
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
